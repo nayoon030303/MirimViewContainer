e37cd149eca2a1aaba37fa3b4e1956fe3caaa219
kny <kny030303>
2020-11-03 20:07:59 +0900

    Complete ViewFlipper Test1

--- app/src/main/java/kr/hs/emirim/s2019s05/mirimviewcontainer/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019s05/mirimviewcontainer/MainActivity.java
@@ -3,12 +3,38 @@ package kr.hs.emirim.s2019s05.mirimviewcontainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+    ViewFlipper flipper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drawer);
+        setContentView(R.layout.activity_filpper);
+
+        flipper = findViewById(R.id.filpper);
+        Button btnPrev = findViewById(R.id.btn_prev);
+        Button btnNext = findViewById(R.id.btn_next);
+        btnPrev.setOnClickListener(btnListener);
+        btnNext.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_prev:
+                    flipper.showPrevious();
+                    break;
+                case R.id.btn_next:
+                    flipper.showNext();
+                    break;
+            }
+
+        }
+    };
 }
